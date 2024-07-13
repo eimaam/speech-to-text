40f9c43a4f3b10e50d838f2fbec2a7fe174f1ba4
eimaam <imamddahir@gmail.com>
2024-07-13 18:29:25 +0100

    HOTFIX: remove checks

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import 'regenerator-runtime/runtime'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
