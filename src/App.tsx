a6e46bce2eab7815a79dc98300b0200e5726134d
eimaam <imamddahir@gmail.com>
2024-07-13 18:10:39 +0100

    HOTFIX: remove linting for testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+//  @ts-nocheck
 import { useEffect, useState } from "react";
 import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import { FaMicrophone, FaStop, FaArrowDown } from "react-icons/fa";
